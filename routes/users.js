9167936dcd73ff643a372145b762e1a2d6e46f00
unknown <m_oleg161@mail.ru>
2020-05-10 00:22:43 +0300

    Rewrite routes with async/await

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,34 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
-const { users } = require('../data/users.js');
+const path = require('path');
+const fs = require('fs');
 
-router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+async function readUsers() {
+  const usersPath = path.join(__dirname, '../data/users.json');
+  try {
+    const data = await fs.promises
+      .readFile(usersPath, { encoding: 'utf-8' });
+    return data;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    return console.error(error);
+  }
+}
+
+
 router.get('/users', (req, res) => {
-  res.send(users);
+  readUsers().then((data) => res.status(200).json(data));
 });
 
 router.get('/users/:_id', (req, res) => {
   // eslint-disable-next-line no-underscore-dangle
-  const userID = req.params._id;
+  const userID = req.params._id * 1;
   // eslint-disable-next-line no-underscore-dangle
-  const userSend = users.find((m) => m._id === userID);
-  if (userSend) {
-    res.status(200).json({ userSend });
+  const userInfo = readUsers().then((data) => data.filter((m) => m._id === userID));
+  if (userInfo) {
+    res.status(200).json({ userInfo });
   } else {
     res.status(400).json({ message: 'Нет пользователя с таким id' });
   }
